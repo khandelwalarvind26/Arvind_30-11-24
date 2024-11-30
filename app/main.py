77b13703d596a298ab5c311efdc18766a8f071d7
Arvind K <khandelwalarvind26@gmail.com>
2024-11-30 11:57:53 +0530

    Removed depreceated function on_event

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from app.api.routes import report
 from app.db import create_tables
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def on_startup():
+# Create tables on startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
     await create_tables()
+    yield  # The point at which the application runs
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(report.router, prefix="/report")
